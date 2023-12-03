f83f6d6f432026d358d39b9b2be24705b92efc88
Dmitriy <123199896+romanaivtrafuse@users.noreply.github.com>
2023-12-03 17:38:12 +0300

    Create localStorage.js

---
+++ localStorage.js
@@ -0,0 +1,6 @@
+// Запись в локальное хранилище
+localStorage.setItem('username', 'John');
+
+// Получение значения из локального хранилища
+const storedUsername = localStorage.getItem('username');
+console.log('Stored username:', storedUsername);
