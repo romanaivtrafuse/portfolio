793b83653fb367c7f29b0867bff0558169ecbc80
Dmitriy <123199896+romanaivtrafuse@users.noreply.github.com>
2023-12-03 17:36:54 +0300

    Create workingAJAX.js

---
+++ workingAJAX.js
@@ -0,0 +1,7 @@
+// Пример асинхронного запроса к серверу
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
