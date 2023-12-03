fb8890804642f99d4b8f3403ab50ac66777721d0
Dmitriy <123199896+romanaivtrafuse@users.noreply.github.com>
2023-12-03 17:35:39 +0300

    Create workingWithObject.js

---
+++ workingWithObject.js
@@ -0,0 +1,16 @@
+// Создание объекта
+let person = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+};
+
+// Доступ к свойствам объекта
+console.log(person.name);
+console.log(person['age']);
+
+// Изменение свойства объекта
+person.age = 31;
+
+// Вывод объекта в консоль
+console.log(person);
