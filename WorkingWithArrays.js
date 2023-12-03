49f7e7701f96862cd28bf748df5b10f35471c80e
Dmitriy <123199896+romanaivtrafuse@users.noreply.github.com>
2023-12-03 17:34:37 +0300

    Create WorkingWithArrays.js

---
+++ WorkingWithArrays.js
@@ -0,0 +1,13 @@
+// Создание массива
+let fruits = ['Apple', 'Banana', 'Orange'];
+
+// Добавление элемента в конец массива
+fruits.push('Grapes');
+
+// Вывод массива в консоль
+console.log(fruits);
+
+// Итерация по массиву
+fruits.forEach(fruit => {
+    console.log(fruit);
+});
